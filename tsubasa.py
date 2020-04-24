445e6a4acf6e530e77f654866b2a2ac54be5e733
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2020-04-24 13:32:59 +0430

    ramadan

--- tsubasa.py
+++ tsubasa.py
@@ -761,9 +761,13 @@ class Tsubasa:
         if CTDT.template("023").click():
             return True
 
-        # go to event match
-        elif CTDT.template("024").click():
-            return True
+        # go to event match if we are in event match page(2) not on event page(1)
+        #  we can detect it by checking if to event exchange shop is not available
+        elif not CTDT.template("061").available():
+
+            # ramadan event
+            if CTDT.template("024").click():
+                return True
 
         # go to story mode - second page
         # use to play story mode only
