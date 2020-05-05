09d9f9bbc283726d851a8038f8486740ebdd5ad9
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2020-05-05 11:31:07 +0430

    fix config

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
         if DEBUG:
             result = tsubasa.run()
             if result is not None:
-                print("Run => {0} : {1}, ".format(result,datetime.now()))
+                print("Run => {0} : {1}".format(result,datetime.now()))
             time.sleep(config.sleep)
         else:
             try:
