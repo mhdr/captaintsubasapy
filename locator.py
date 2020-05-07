47491e9af9a1da99174e2d1d9603b79044940e77
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2020-05-07 12:52:11 +0430

    change mode

--- locator.py
+++ locator.py
@@ -16,11 +16,11 @@ threshold = 0.97
 
 # file name like : 031f.jpg
 # Column A in excel
-index_fimage = 78
+index_fimage = 6
 
 # template number like : 007.jpg
 # Column F in excel
-index_template = 78
+index_template = 6
 
 # first match = 0
 match = 0
