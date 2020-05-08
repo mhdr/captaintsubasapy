eb944d7e0d386793b9aa8cc009744dcbe8adf7e5
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2020-05-08 12:47:25 +0430

    3rd anniversary event

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,22 @@
 from PIL import Image
 import pytesseract
+import cv2
 
 # https://stackoverflow.com/questions/50951955/pytesseract-tesseractnotfound-error-tesseract-is-not-installed-or-its-not-i
+# https://github.com/UB-Mannheim/tesseract/wiki
+# https://www.geeksforgeeks.org/python-string-isnumeric-application/
+# https://stackoverflow.com/questions/19580102/inverting-image-in-python-with-opencv
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
 
-print(pytesseract.image_to_string(Image.open('ocr.png')))
+image = cv2.imread("ocr.png")
+gs_imagem = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+data = pytesseract.image_to_string(cv2.bitwise_not(gs_imagem))
+new_data = ""
+
+for d in data:
+    if d.isnumeric():
+        new_data += d
+
+print(new_data)
--- tsubasa.py
+++ tsubasa.py
@@ -836,36 +836,36 @@ class Tsubasa:
 
         if self.config.mode not in modes: return False
 
-        # # go to event
-        # if CTDT.template("023").click():
-        #     return True
-        #
-        # # go to event match if we are in event match page(2) not on event page(1)
-        # #  we can detect it by checking if to event exchange shop is not available
-        # elif not CTDT.template("061").available():
-        #
-        #     # ramadan event
-        #     if CTDT.template("024").click():
-        #         return True
+        # go to event
+        if CTDT.template("023").click():
+            return True
+
+        # go to event match if we are in event match page(2) not on event page(1)
+        #  we can detect it by checking if to event exchange shop is not available
+        elif not CTDT.template("061").available():
+
+            # ramadan event
+            if CTDT.template("024").click():
+                return True
 
         ##########################################################
-        # go to story mode - second page
-        # use to play story mode only
-        if CTDT.template("004").click():
-            return True
-
-        # begin scroll
-        # if in story mode and if we are at the beginning of the scroll ( scrollbar is on left )
-        # we should scroll to right
-        elif CTDT.template("005").available():
-            CTDT.point("001").click(clicks=5, interval=0.1)
-            return True
-
-        # end scroll
-        # exact game picture ( it should change always to match)
-        # for now -> story mode - evolve -> get drills all types
-        elif CTDT.template("006").click():
-            return True
+        # # go to story mode - second page
+        # # use to play story mode only
+        # if CTDT.template("004").click():
+        #     return True
+        #
+        # # begin scroll
+        # # if in story mode and if we are at the beginning of the scroll ( scrollbar is on left )
+        # # we should scroll to right
+        # elif CTDT.template("005").available():
+        #     CTDT.point("001").click(clicks=5, interval=0.1)
+        #     return True
+        #
+        # # end scroll
+        # # exact game picture ( it should change always to match)
+        # # for now -> story mode - evolve -> get drills all types
+        # elif CTDT.template("006").click():
+        #     return True
 
         ##########################################################
         # go to evolve player
