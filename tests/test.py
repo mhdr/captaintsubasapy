a00730ca832b20774f85c05ec878e85214a7ac0d
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2020-05-18 15:20:58 +0430

    test

--- tests/test.py
+++ tests/test.py
@@ -1,22 +1,12 @@
-import pytesseract
-import cv2
+from random import seed
+from random import random
 
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
+# seed random number generator
+seed(1)
+# generate some random numbers
+print(random(), random(), random())
 
-image = cv2.imread("ocr.png")
-image_grayscal = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-inverted_image = cv2.bitwise_not(image_grayscal) # color of text is white so we should invert colors
-
-data = pytesseract.image_to_string(inverted_image)
-new_data = ""
-
-for d in data:
-    if d.isnumeric():
-        new_data += d
-
-print(new_data)
-
-
-
-# https://stackoverflow.com/questions/50951955/pytesseract-tesseractnotfound-error-tesseract-is-not-installed-or-its-not-i
-# https://github.com/UB-Mannheim/tesseract/wiki
+# reset the seed
+seed(1)
+# generate some random numbers
+print(random(), random(), random())
