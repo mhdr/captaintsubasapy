cc37af859959248ad71119c7698549d297ba4cd5
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2020-06-03 18:10:00 +0430

    annivesary

--- locator.py
+++ locator.py
@@ -16,11 +16,11 @@ threshold = 0.97
 
 # file name like : 031f.jpg
 # Column A in excel
-index_fimage = 23
+index_fimage = 60
 
 # template number like : 007.jpg
 # Column F in excel
-index_template = 23
+index_template = 60
 
 # first match = 0
 match = 0
